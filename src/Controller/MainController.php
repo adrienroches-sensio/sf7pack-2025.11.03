2fbccb6919143eafca50ed079d195a8abd860ca3
Adrien Roches <adrien@roc-it.tech>
2025-11-04 11:31:48 +0100

    [sf_start] Use 'name' query parameter from the request

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,8 +16,10 @@ class MainController extends AbstractController
         name: 'app_index',
         methods: ['GET']
     )]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new Response('Hello World!');
+        $name = $request->query->get('name', 'World');
+
+        return new Response("Hello {$name}!");
     }
 }

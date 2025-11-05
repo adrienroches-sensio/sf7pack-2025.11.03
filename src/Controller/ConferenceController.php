28ce924c1c950832555cc1fc0daa2fcf60d491a8
Adrien Roches <adrien@roc-it.tech>
2025-11-05 09:39:18 +0100

    [sf_start] Update the conference controller to fill in comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,14 +5,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Conference;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference/{name}/{start}/{end}', name: 'app_conference_new')]
-    public function newConference(string $name, string $start, string $end, /* ... */): Response
+    #[Route(
+        path: '/conference/{name}/{start}/{end}',
+        name: 'app_conference_new',
+        requirements: [
+            'name' => Requirement::ASCII_SLUG,
+            'start' => Requirement::DATE_YMD,
+            'end' => Requirement::DATE_YMD,
+        ]
+    )]
+    public function newConference(string $name, string $start, string $end, EntityManagerInterface $em): Response
     {
         $conference = (new Conference())
             ->setName($name)
@@ -22,8 +32,9 @@ class ConferenceController extends AbstractController
             ->setEndAt(new \DateTimeImmutable($end))
         ;
 
-        //...
+        $em->persist($conference);
+        $em->flush();
 
-        return new Response('Conference created');
+        return new Response("Conference {$conference->getId()} created");
     }
 }

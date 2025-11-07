436cb3e2848fa30fd9d56aa50d506c20d0a54199
Adrien Roches <adrien@roc-it.tech>
2025-11-07 13:50:02 +0100

    [sf_advanced] Add createdBy using make:entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -30,6 +32,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    /**
+     * @var Collection<int, Conference>
+     */
+    #[ORM\OneToMany(targetEntity: Conference::class, mappedBy: 'createdBy', orphanRemoval: true)]
+    private Collection $conferences;
+
+    public function __construct()
+    {
+        $this->conferences = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -110,5 +123,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials(): void
     {
         // @deprecated, to be removed when upgrading to Symfony 8
+    }
+
+    /**
+     * @return Collection<int, Conference>
+     */
+    public function getConferences(): Collection
+    {
+        return $this->conferences;
+    }
+
+    public function addConference(Conference $conference): static
+    {
+        if (!$this->conferences->contains($conference)) {
+            $this->conferences->add($conference);
+            $conference->setCreatedBy($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConference(Conference $conference): static
+    {
+        if ($this->conferences->removeElement($conference)) {
+            // set the owning side to null (unless already changed)
+            if ($conference->getCreatedBy() === $this) {
+                $conference->setCreatedBy(null);
+            }
+        }
+
+        return $this;
     }
 }
